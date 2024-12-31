508ec0daf81c7aadb445870b4a82b785c7e77c3a
IgorBayerl <dev.igorbayerl@gmail.com>
2024-12-31 15:44:55 +0100

    declare signals in code

---
+++ dependencies/dependencies.md
@@ -0,0 +1,11 @@
+Here we add the godot engine as a dependency
+
+It need to be named 
+
+
+can be downloaded from https://godotengine.org/download
+```
+- dependencies
+  - Godot.exe
+  - Godot_console.exe
+  - /GodotSharp
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,20 +4,39 @@ const GAME_SCENE = "res://scenes/game.tscn"
 
 func _ready():
 	print("Main menu ready...")
+	
+	# Check for dedicated server and host game automatically
 	if OS.has_feature("dedicated_server"):
 		print("Calling host game...")
 		NetworkManager.host_game()
-		get_tree().call_deferred(&"change_scene_to_packed", preload(GAME_SCENE))
+		get_tree().call_deferred("change_scene_to_packed", preload(GAME_SCENE))
+	
+	# Connect signals for buttons
+	$Menu/Exit.pressed.connect(_on_exit_pressed)
+	$Menu/HostGame.pressed.connect(_on_host_game_pressed)
+	$Menu/JoinGame.pressed.connect(_on_join_game_pressed)
+
+# Callback for the Exit button
+func _on_exit_pressed():
+	exit_game()
+
+# Callback for the Host Game button
+func _on_host_game_pressed():
+	host_game()
+
+# Callback for the Join Game button
+func _on_join_game_pressed():
+	join_game()
 
 func host_game():
 	print("Host game pressed")
 	NetworkManager.host_game()
-	get_tree().call_deferred(&"change_scene_to_packed", preload(GAME_SCENE))
-	
+	get_tree().call_deferred("change_scene_to_packed", preload(GAME_SCENE))
+
 func join_game():
 	print("Join game pressed")
 	NetworkManager.join_game()
-	get_tree().call_deferred(&"change_scene_to_packed", preload(GAME_SCENE))
-	
+	get_tree().call_deferred("change_scene_to_packed", preload(GAME_SCENE))
+
 func exit_game():
 	get_tree().quit(0)
